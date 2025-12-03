c069545edba066ac18260d99ef3cdb9dbdb229af
MD ARAFAT ANOWAR <arafat-anowar@outlook.com>
2025-12-04 00:58:43 +0600

    Module-2.5

---
+++ Semester 1/C++ for DSA/Module 2.5/C_Simple_Calculator.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << x << " + " << y << " = " << (long long int)x + y << endl
+         << x << " * " << y << " = " << (long long int)x * y << endl
+         << x << " - " << y << " = " << x - y;
+    return 0;
+}
